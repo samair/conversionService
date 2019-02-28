ababb2984735f21e1a23dfd318221cb84890c761
samair <tsameerc@gmail.com>
2019-02-28 23:56:53 +0530

    travis support added

---
+++ src/test/java/com/webvidhi/microservices/forex/conversionService/ConversionServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.webvidhi.microservices.forex.conversionService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConversionServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
